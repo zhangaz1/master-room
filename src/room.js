bdaf24e8ef86810795e88d6553e7846a59dcb52a
zhangaz1 <zhangaz1@hotmail.com>
2017-11-18 19:19:15 +0800

    room init a name

--- src/room.js
+++ src/room.js
@@ -1,6 +1,6 @@
 class Room {
-	constructor() {
-
+	constructor(name) {
+		this.name = name;
 	}
 }
 
--- test/room.test.js
+++ test/room.test.js
@@ -5,10 +5,15 @@ let should = require('should');
 
 let Room = require('./../src/room.js');
 
-describe('test Room', function() {
+describe('test Room', function () {
+	const roomName = 'test room';
 
-	it('should can new a Room', function() {
-		const room = new Room();
+	it('should can new a Room', function () {
+		const room = new Room(roomName);
+
+		room.should.be.an.Object()
+			.which.has.property('name')
+			.be.exactly(roomName);
 	});
 
 });
